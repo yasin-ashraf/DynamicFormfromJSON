f21deda32f4010160aaf5971febf1cce36200ef2
yasin-ashraf <yasin.asharf@gmail.com>
2018-11-17 12:17:24 +0530

    Delete Report

--- app/src/main/java/com/yasin/hubbler/Activity/ViewReportActivity.java
+++ app/src/main/java/com/yasin/hubbler/Activity/ViewReportActivity.java
@@ -1,17 +1,27 @@
 package com.yasin.hubbler.Activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.yasin.hubbler.DatabaseClient;
 import com.yasin.hubbler.Fragment.EditReportFragment;
 import com.yasin.hubbler.Fragment.ViewReportFragment;
+import com.yasin.hubbler.Hubbler;
+import com.yasin.hubbler.Model.Report;
 import com.yasin.hubbler.R;
 
 import org.json.JSONException;
@@ -31,6 +41,7 @@ public class ViewReportActivity extends AppCompatActivity implements View.OnClic
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private TextView firstLetter;
     private FloatingActionButton editButton;
+    private FloatingActionButton deleteButton;
     private AppBarLayout appBarLayout;
 
     @Override
@@ -48,6 +59,7 @@ public class ViewReportActivity extends AppCompatActivity implements View.OnClic
         collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
         firstLetter = findViewById(R.id.tv_property_first_letter);
         editButton = findViewById(R.id.fab_edit);
+        deleteButton = findViewById(R.id.fab_delete);
         appBarLayout = findViewById(R.id.appBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -55,6 +67,7 @@ public class ViewReportActivity extends AppCompatActivity implements View.OnClic
 
         showFields();
         editButton.setOnClickListener(this);
+        deleteButton.setOnClickListener(this);
     }
 
     public void setReport(String report) {
@@ -107,6 +120,35 @@ public class ViewReportActivity extends AppCompatActivity implements View.OnClic
         }
     }
 
+    private void deleteReport(){
+        Hubbler.getApp(this).getExecutor().execute(() -> {
+            Report report = new Report();
+            report.setReport(getReport());
+            report.setId(id);
+            DatabaseClient.getInstance(ViewReportActivity.this).getAppDatabase().reportDao().delete(report);
+            finish();
+            runOnUiThread(() -> Toast.makeText(ViewReportActivity.this, R.string.toast_report_deleted, Toast.LENGTH_SHORT).show());
+        });
+    }
+
+    private void showDeleteDialog(){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage(R.string.label_are_you_sure);
+        builder1.setCancelable(true);
+        builder1.setPositiveButton(
+                R.string.label_yes_delete,
+                (dialog, id) -> {
+                    deleteReport();
+                });
+
+        builder1.setNegativeButton(
+                R.string.label_no_dont,
+                (dialog, id) -> dialog.cancel());
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -114,6 +156,10 @@ public class ViewReportActivity extends AppCompatActivity implements View.OnClic
                 showEditFields();
                 editButton.hide();
                 appBarLayout.setExpanded(false,true);
+                break;
+
+            case R.id.fab_delete:
+                showDeleteDialog();
                 break;
         }
     }
